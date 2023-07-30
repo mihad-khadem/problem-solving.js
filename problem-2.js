955efe6a9392201c42951312e3459a1594c932a5
mihad-khadem <mihadkhadem@gmail.com>
2023-07-31 04:21:32 +0600

    Problem 2 completed

---
+++ problem-2.js
@@ -0,0 +1,25 @@
+var mehedi = 9;
+var mahmud = 8;
+var hasan = 66;
+var rakib = 5;
+var forhad =30;
+
+if(mehedi >= 90){
+    console.log("Grade : A+" )
+}
+else if(mahmud >= 80){
+    console.log("Grade : A")
+}
+else if (hasan >= 70){
+    console.log("Grade : B")
+}
+else if(rakib >= 60){
+    console.log("Grade : C")
+}
+else if(forhad >= 40){
+    console.log('Grade : D')
+}
+else {
+    console.log("F++")
+}
+
