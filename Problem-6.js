b0d36774991ad0bef083a33cc686ec56e4acb203
mihad-khadem <mihadkhadem@gmail.com>
2023-08-01 23:53:34 +0600

    Problem 6 solved

---
+++ Problem-6.js
@@ -0,0 +1,10 @@
+// Triangle with the sides 9, 8, 9
+// Write a program to check whether a triangle is Isosceles or not.
+
+var side_1 = 9;
+var side_2 = 8;
+var side_3 = 9;
+
+if(side_1 === side_2 || side_2 === side_3 || side_1 === side_3){
+    console.log('Isosceles');
+}
