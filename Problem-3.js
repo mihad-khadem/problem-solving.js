e1c1b514f491e930fca85de2eb1e337484ab1127
mihad-khadem <mihadkhadem@gmail.com>
2023-08-01 19:35:20 +0600

    Problem 3 Solved

---
+++ Problem-3.js
@@ -0,0 +1,9 @@
+var momGave = 1000;
+console.log('Mom Gave me = ' + momGave + ' TK');
+var applePrice = 360;
+var orangePrice = 340;
+
+var total = applePrice + orangePrice;
+console.log('Total = ' + total + ' TK');
+var shopkeeperReturn = momGave - total;
+console.log('Shop Keeper Return = ' + shopkeeperReturn + ' TK');
